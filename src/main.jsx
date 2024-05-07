0eff5b371489cfbeb16f8a178bdc64eb3362f290
hbfrzk <arzaak232456@gmail.com>
2024-05-07 19:52:54 +0800

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./index.css";
+import FirstPage from "./pages/first.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<FirstPage />} />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
---
+++ src/pages/first.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FirstPage() {
+  return <div>Login</div>;
+}
+
+export default FirstPage;
